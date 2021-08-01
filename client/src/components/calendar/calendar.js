0e5217962607f5a3020e3ee27bf1d5e8a5b426af
Troy Freed <tfreed94@gmail.com>
2021-08-01 18:53:43 -0400

    installed moment.js and set up the imports for it

--- client/src/components/calendar/calendar.js
+++ client/src/components/calendar/calendar.js
@@ -1,6 +1,9 @@
 import React from "react";
+import moment from 'moment'
 
 const Calendar = () => {
+    const value = moment()
+    const startDay = value
     return (
         <div>Calendar</div>
 
