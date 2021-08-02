53f55af50ac2bca17515d45205925bc59a9f0b95
Troy Freed <tfreed94@gmail.com>
2021-08-01 21:36:44 -0400

    rendered days of the week horizontally with css

--- client/src/components/calendar/calendar.js
+++ client/src/components/calendar/calendar.js
@@ -1,13 +1,28 @@
 import React from "react";
 import moment from 'moment'
+import "./styles.css";
 
 const Calendar = () => {
     const value = moment()
     const startDay = value.clone().startOf("month").startOf("week");
     const endDay = value.clone().endOf("month");
+    const day = startDay.clone().subtract(1, "day");
+    const calendar = []
+
+    while (day.isBefore(endDay, "day")) {
+        calendar.push(
+            Array(7).fill(0).map(() => day.add(1, "day").clone())
+        )
+    }
     return (
-        <div>
-            {startDay.format("MM/DD")} - {endDay.format("MM/DD")}
+        <div className="calendar">
+            {calendar.map((week => 
+            <div>
+                    {week.map((day => 
+                    <div className="day">{day.format("D")}</div>
+                    ))}
+                </div>
+                ))}
         </div>
 
     );
