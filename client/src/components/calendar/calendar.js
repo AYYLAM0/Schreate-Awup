b66c7deb1ef452b58b0a034e3c1a85001e0537bc
Troy Freed <tfreed94@gmail.com>
2021-08-02 12:14:34 -0400

    removed build file because throwing error

--- client/src/components/calendar/calendar.js
+++ client/src/components/calendar/calendar.js
@@ -6,8 +6,18 @@ const Calendar = () => {
     const [calendar, setCalender] = useState([]);
     const [value, setValue] = useState(moment());
 
-    useEffect(() => {
+    const startDay = value.clone().startOf("month").startOf("week");
+    const endDay = value.clone().endOf("month");
 
+
+    useEffect(() => {
+        const day = startDay.clone().subtract(1, "day");
+        const a = []
+        while (day.isBefore(endDay, "day")) {
+            a.push(
+                Array(7).fill(0).map(() => day.add(1, "day").clone())
+            )
+        }
         setCalender(a);
     }, [value])
 
