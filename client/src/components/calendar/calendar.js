be6ee9ce6e563c3baa239f936c6ea3f17bd1dcea
Troy Freed <tfreed94@gmail.com>
2021-08-01 21:43:32 -0400

    added useEffect to stop too many renders error

--- client/src/components/calendar/calendar.js
+++ client/src/components/calendar/calendar.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import moment from 'moment'
-import "./styles.css";
+import "./calender.css";
 
 const Calendar = () => {
-    const value = moment()
+    const [calendar, setCalender] = useState([]);
+    const [value, setValue] = useState(moment())
+
     const startDay = value.clone().startOf("month").startOf("week");
     const endDay = value.clone().endOf("month");
     const day = startDay.clone().subtract(1, "day");
-    const calendar = []
-
+    
+    const a = []
     while (day.isBefore(endDay, "day")) {
         calendar.push(
             Array(7).fill(0).map(() => day.add(1, "day").clone())
         )
     }
+    setCalender(a);
     return (
         <div className="calendar">
             {calendar.map((week => 
